56b739162ed664aa9f1ffe4b0488ca21de1dff55
Sebastian Leu <sebastian.leu@protonmail.com>
2020-08-01 12:07:17 +0700

    Adapted all usages of lndo.site urls to use local.host domain instead

--- scripts/site-install.sh
+++ scripts/site-install.sh
@@ -2,4 +2,4 @@
 
 drush --root=/app/web si --db-url=mysql://drupal8:drupal8@database/drupal8 -y $1
 find /app/web/sites/default -type d -exec chmod 777 '{}' \;
-drush --root=/app/web --uri=https://drupal-contributions.lndo.site uli
+drush --root=/app/web --uri=https://drupal-contributions.local.host uli
